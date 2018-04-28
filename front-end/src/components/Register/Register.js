0bff853e3a35e5b093ffb5903780c8d1699ffb36
nofcaz <nofcaz@protonmail.com>
2018-04-28 00:24:49 -0400

    Update front-end with heroku

--- front-end/src/components/Register/Register.js
+++ front-end/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignin = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://morning-wave-39311.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
